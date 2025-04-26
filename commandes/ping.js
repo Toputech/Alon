fb7ce87c91b1af1e2c694f9e53b977f7cb9a3849
Toputech <168991681+Toputech@users.noreply.github.com>
2025-04-26 09:32:15 +0300

    Initial commit

---
+++ commandes/ping.js
@@ -0,0 +1,72 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+const util = require('util');
+const fs = require('fs-extra');
+const axios = require('axios');
+const { zokou } = require(__dirname + "/../framework/zokou");
+const os = require("os");
+const moment = require("moment-timezone");
+const conf = require(__dirname + "/../set");
+
+const AUDIO_URL = "https://files.catbox.moe/hrf957.mp3"; // New audio URL
+const THUMBNAIL_URL = "https://files.catbox.moe/yedfbr.jpg"; // New image URL
+
+moment.tz.setDefault(`${conf.TZ}`);
+
+const getTimeAndDate = () => {
+    return {
+        time: moment().format('HH:mm:ss'),
+        date: moment().format('DD/MM/YYYY')
+    };
+};
+
+// Ping Command
+zokou({ nomCom: "ping", categorie: "General" }, async (dest, zk, commandeOptions) => {
+    let { ms } = commandeOptions;
+    const { time, date } = getTimeAndDate();
+    const ping = Math.floor(Math.random() * 1000) + 1; // Generate a random ping between 1ms - 100ms
+
+    try {
+    await zk.sendMessage(dest, {
+        audio: { url: AUDIO_URL }, 
+            mimetype: 'audio/mp4', 
+            ptt: true, // Voice note form
+      text: `Pong...: ${ping}ms\n🍒🍓`,
+      contextInfo: {
+        forwardingScore: 999,
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: {
+              newsletterJid: '120363295141350550@newsletter',
+              newsletterName: 'ALONE Queen MD V²',
+              serverMessageId: 143},
+        externalAdReply: {
+          
+          title: "Follow for updates 🩸",
+      body: "Enjoy...",
+      thumbnailUrl: conf.URL,
+          sourceUrl: conf.GURL,
+          mediaType: 1,
+          
+        }
+      }
+    }, { quoted: ms });
+
+    await zk.sendMessage(dest, {
+        text: "```Testing ping..✓```"
+    } ,{ quoted: ms });// Voice note form
+    }catch (e) {
+        console.log("❌ Ping Command Error: " + e);
+        repondre("❌ Error: " + e);
+    }
+});
